30e1876eaf36c0f10a507d00a70e00fbe14e4f41
DonnyStout <donnystout47@gmail.com>
2018-02-07 20:43:45 -0700

    Added comments for both Craps and Game, also filled in the README.md file.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 
 
 ![Craps Screen Capture](Craps.png)
+
+
+Allows the user to play a single game of craps or to run a simulation of
+Monte Carlo style that will continue to run indefinitely as long as the 
+user has the run button deployed.
+
+The application projects images of dice faces depending on the rolls
+given and displays the sum of each roll. If the final roll is a win
+the application will output a green color to signify win and a red 
+color to signify a loss.
+
+A tally at the bottom of the screen displays total wins divided by
+total plays to output the percentage of the wins against losses.
+
+The buttons are play for a single play. The run button if activated
+will deploy and play indefinitely until engaged again. Reset allows the
+user to clear the current tally and the dice in the display from the last
+play.
--- app/src/main/java/edu/cnm/deepdive/craps/models/Game.java
+++ app/src/main/java/edu/cnm/deepdive/craps/models/Game.java
@@ -2,17 +2,34 @@ package edu.cnm.deepdive.craps.models;
 
 import edu.cnm.deepdive.craps.models.Craps.State;
 
+/**
+ * The <code>Game</code> class invokes methods from the <code>Craps</code> class to allow the
+ * user to run the Monte Carlo styled simulation of Craps.
+ *
+ * Author Donny Stout
+ */
 public class Game {
 
   private Craps craps;
   private long wins;
   private long losses;
 
+  /**
+   * Creates a new instance of the Craps class and invokes the reset method employed in the
+   * <code>Craps</code> class.
+   */
   public Game() {
     craps = new Craps();
     reset();
   }
 
+  /**
+   * Allows for keeping track of wins and losses by checking the <code>State</code> updating the
+   * result of play and if <code>WIN</code> increments wins and if <code>LOSS</code> increments
+   * losses.
+   *
+   * @return                      The result of the state after incrementing the result.
+   */
   public Craps.State play() {
     Craps.State result = craps.play();
     if (result == State.WIN) {
@@ -23,20 +40,39 @@ public class Game {
     return result;
   }
 
+  /**
+   * Allows for the ability to reset the tally of accumulated wins and losses by invoking reset
+   * implemented in the <code>Craps</code> class.
+   */
   public void reset() {
    wins = 0;
    losses = 0;
    craps.reset();
   }
 
+  /**
+   * Employs an instance of <code>Craps</code> to allow for access to the methods.
+   *
+   * @return                   Returns craps to the field.
+   */
   public Craps getCraps() {
     return craps;
   }
 
+  /**
+   * Allows access to receive the wins tally from the <code>Craps</code> class.
+   *
+   * @return              Returns wins to the field.
+   */
   public long getWins() {
     return wins;
   }
 
+  /**
+   * Allows access to receiver the tally of losses from the <code>Craps</code> class.
+   *
+   * @return                Returns losses to the field.
+   */
   public long getLosses() {
     return losses;
   }
